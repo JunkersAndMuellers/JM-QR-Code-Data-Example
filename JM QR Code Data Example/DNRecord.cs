06609593fd4aa0883e630e6e76a7293ecabe8948
Axel Stallknecht <axel.stallknecht@jm-textile.com>
2021-04-12 20:21:13 +0200

    DNRecord added

---
+++ JM QR Code Data Example/DNRecord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JM.Tools
+{
+    [Serializable]
+    public class DNRecord
+    {
+        public int DeliveryNoteNo { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerStreet { get; set; }
+        public string CustomerCountry { get; set; }
+        public string CustomerZipcode { get; set; }
+        public string CustomerCity { get; set; }
+        public string DeliveryName { get; set; }
+        public string DeliveryStreet { get; set; }
+        public string DeliveryCountry { get; set; }
+        public string DeliveryZipcode { get; set; }
+        public string DeliveryCity { get; set; }
+        public string ContactName { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactMail { get; set; }
+        public int PieceCount { get; set; }
+        public double TotalWeight { get; set; }
+        public List<PieceRecord> Pieces { get; set; }
+    }
+}
